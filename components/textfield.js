9572e9b25a65b5c69512f59833b4cc1a619e76c3
lorengreenfield <loren@intereact.co.nz>
2017-11-27 22:10:44 +1300

    Added stylesheet for global styles

--- components/textfield.js
+++ components/textfield.js
@@ -1,4 +1,4 @@
-import {html, css, formField, formIsValid, attribute} from 'halfcab'
+import { html, css, formField, formIsValid, attribute, fieldIsTouched, rerender } from 'halfcab'
 
 let styles = css`
     .textfield {
@@ -9,16 +9,25 @@ let styles = css`
       margin-top: 10px;
       width: 80%;
       font-size: 18px;
+      border-radius: 0;
+      box-shadow: none !important;
     }
     
-    .textfield:focus,
-    .textfield.focus {
+    .textfield:focus {
       border: solid 5px #969696;
+    }
+    
+    .textfield::placeholder {
+      color: #999;
+    }
+    
+    .textfield.touched:invalid:not(:focus) {
+      border-color: red;
     }
 `
 
 export default ({holdingPen, name, property, required, pattern, type, keyup, autofocus}) => html`
 
-    <input class="${styles.textfield}" value="${holdingPen[property]}" onfocus=${e => e.currentTarget.classList.add('sf-touched')} onkeyup=${e => keyup(e)} ${required ? `required` : ''} oninput=${formField(holdingPen, property)} placeholder="${name || ''}" type="${type}" style="${!formIsValid(holdingPen) ? '' : ''}" ${attribute(autofocus ? 'autofocus' : '')} />
+    <input class="${styles.textfield} ${fieldIsTouched(holdingPen, property) === true ? styles.touched : ''}" value="${holdingPen[property]}" onkeyup=${e => keyup(e)} ${required ? `required` : ''} oninput=${formField(holdingPen, property)} onblur=${formField(holdingPen, property)} placeholder="${name || ''}" type="${type}" style="${!formIsValid(holdingPen) ? '' : ''}" ${attribute(autofocus ? 'autofocus' : '')} />
     
 `
