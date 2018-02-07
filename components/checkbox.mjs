061fea159e96e260326194f895c1f377fad4980f
lorengreenfield <loren@intereact.co.nz>
2018-02-08 00:08:01 +1300

    Better aligned check boxes

--- components/checkbox.mjs
+++ components/checkbox.mjs
@@ -6,13 +6,14 @@ let styles = css`
   }
   
   .label {
+    display: inline-flex; 
+    align-items: center;
     border-radius: 5px;
     font-size: 16px; 
     font-weight: normal; 
     color: #999; 
     margin-left: 5px; 
-    padding: 14px; 
-    v-align: top; 
+    padding: 14px;
     background-color: rgba(255,255,255,0.8);
     box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
   }
