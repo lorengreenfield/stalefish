6cbf34f1f6486b198df1dcfaf3ec74399352ad8b
lorengreenfield <loren@intereact.co.nz>
2018-05-19 20:42:26 +1200

    added thinBorder optio to panels

--- components/panel.mjs
+++ components/panel.mjs
@@ -30,7 +30,7 @@ let styles = css`
   }
 `
 
-export default ({chevronStyle = null, on = true, content = '', heading = '', closeAction = null, collapseToggleAction = null, collapsed = false, headerAction = null}) => html`${on ? html`<div class="${styles.panel}" style='box-sizing: border-box; position: relative; top: 0px; left: 0px; text-align: left; box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12); border: solid 5px #4371AD; background-color: #FBFBFB;'><div style="position: relative;">
+export default ({chevronStyle = null, on = true, content = '', heading = '', closeAction = null, collapseToggleAction = null, collapsed = false, headerAction = null, thinBorder = false}) => html`${on ? html`<div class="${styles.panel}" style='box-sizing: border-box; position: relative; top: 0px; left: 0px; text-align: left; box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12); border: solid ${thinBorder ? '3px' : '5px'} #4371AD; background-color: #FBFBFB;'><div style="position: relative;">
   ${heading ? html`<div class="${styles.header}" onclick=${() => headerAction && headerAction()}>
     <h2 style="margin: 12px; user-select: none;">${heading}</h2>
   </div></div>` : ''}
