710b806063fe22a4bc50e9ba9120f23cf2d9dbcd
lorengreenfield <loren@intereact.co.nz>
2018-05-20 22:49:58 +1200

    Added permanentTopPlaceholder boolena to textfields and text boxes

--- components/textfield.mjs
+++ components/textfield.mjs
@@ -86,11 +86,11 @@ function determineStep(type){
   return '1'
 }
 
-const textfield = ({wrapperStyle = null, holdingPen, label, placeholder, property, required, pattern, type, keyup, autofocus, valueContext}) => html`
+const textfield = ({wrapperStyle = null, holdingPen, label, placeholder, property, required, pattern, type, keyup, autofocus, valueContext, permanentTopPlaceholder = false}) => html`
 <div ${wrapperStyle ? {'class': wrapperStyle} : ''}>
    <label style="width: 100%; text-align: left; position: relative;">
    ${valueContext ? html`<div class="${styles.valueContext}">${valueContext}</div>` : ''}
-   ${label ? html`<span class="${styles.label}" style="opacity: ${holdingPen[property] === 0 || holdingPen[property] ? 1 : 0}; font-size: 16px; font-weight: normal; color: #999; margin-left: 5px; padding: 9px; background-color: rgba(255,255,255,0.8); ">${label}${required ? ' *' : ''}</span>`:''}
+   ${label ? html`<span class="${styles.label}" style="opacity: ${holdingPen[property] === 0 || holdingPen[property] || permanentTopPlaceholder ? 1 : 0}; font-size: 16px; font-weight: normal; color: #999; margin-left: 5px; padding: 9px; background-color: rgba(255,255,255,0.8); ">${label}${required ? ' *' : ''}</span>`:''}
    <input class="${styles.textfield} ${fieldIsTouched(holdingPen, property) === true ? styles.touched : ''}" value="${holdingPen[property] || ''}" onkeyup=${e => keyup && keyup(e)} ${required ? {required: 'required'} : ''} oninput=${e => change({e, holdingPen, property, label: styles.label})} onblur=${formField(holdingPen, property)} placeholder="${placeholder || ''}${required ? ' *' : ''}" type="${determineType(type)}" ${autofocus ? {autofocus} : ''}  ${pattern ? {pattern} : ''} ${type.toLowerCase() === 'number' ? {step: determineStep(type)} : ''} />
    </label>
 </div>
