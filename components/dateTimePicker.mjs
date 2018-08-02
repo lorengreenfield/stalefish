17b0246ad50d6b8419188c2a01aa85b589a22c65
lorengreenfield <loren@intereact.co.nz>
2018-08-02 18:52:47 +1200

    Changed datePicker to dateTimePicker so as to add time only functionality

--- components/dateTimePicker.mjs
+++ components/dateTimePicker.mjs
@@ -2,6 +2,7 @@ import { html, css, formField, fieldIsTouched, cache } from 'halfcab'
 import flatpickr from 'flatpickr'
 import hash from 'hash-it'
 import calendarIcon from './calendarIcon'
+import timeIcon from './timeIcon'
 
 let styles = css`
   .textfield {
@@ -46,25 +47,11 @@ let styles = css`
     border-color: red;
   }
   
-  .valueContext {
-    position: absolute;
-    color: #AAA;
-    font-size: 1.1em; 
-    line-height: 1.2em;
-    font-weight: normal; 
-    box-sizing: border-box;
-    top: -12px; 
-    right: 7px; 
-    background-color: #EEE; 
-    padding: 10px;
-    z-index: 30;
-  }
-  
   .icon {
     position: absolute;
     pointer-events: none;
     color: #c9c9c9;
-    right: 17px;
+    right: 12px;
     top: -9px;
     font-size: 2em;
     z-index: 30;
@@ -77,11 +64,11 @@ function change ({e, holdingPen, property, label}) {
   return ff
 }
 
-const datePicker = ({wrapperStyle = null, onkeyup, holdingPen, label, placeholder, property, required, pattern, autofocus, permanentTopPlaceholder = true, onchange, oninput, flatpickrConfig = {}}) => {
+const dateTimePicker = ({wrapperStyle = null, holdingPen, label, placeholder, property, required, pattern, autofocus, permanentTopPlaceholder = true, onchange, oninput, flatpickrConfig = {}, timeOnly = false}) => {
   let el = html`
   <div ${wrapperStyle ? {'class': wrapperStyle} : ''} style="min-height: 55px; display: inline-block; width: calc(100% - 10px); margin: 40px 5px 5px 5px;">
      <label style="width: 100%; text-align: left; position: relative; padding: 0;">
-     <div class="${styles.icon}" data-toggle>${calendarIcon({colour: '#ccc'})}</div>
+     <div class="${styles.icon}" data-toggle>${timeOnly ? timeIcon({colour: '#ccc'}) : calendarIcon({colour: '#ccc'})}</div>
      ${label ? html`<span class="${styles.label}" style="opacity: ${holdingPen[property] === 0 || holdingPen[property] || permanentTopPlaceholder ? 1 : 0}; font-size: 16px; font-weight: normal; color: #999; margin-left: 5px; padding: 9px; background-color: rgba(255,255,255,0.8); ">${label}${required ? ' *' : ''}</span>` : ''}
      <input class="${styles.textfield} ${fieldIsTouched(holdingPen, property) === true ? styles.touched : ''}" value="${holdingPen[property] || ''}" onkeyup=${e => onkeyup && onkeyup(e)} ${required ? {required: 'required'} : ''} onchange=${e => { change({e, holdingPen, property, label: styles.label}); onchange && onchange(e) }} oninput=${e => { change({e, holdingPen, property, label: styles.label}); oninput && oninput(e) }} onblur=${formField(holdingPen, property)} placeholder="${placeholder || ''}${required ? ' *' : ''}" type="text" ${autofocus ? {autofocus} : ''}  ${pattern ? {pattern} : ''}  data-input />
      </label>
@@ -91,10 +78,10 @@ const datePicker = ({wrapperStyle = null, onkeyup, holdingPen, label, placeholde
   if (typeof window !== 'undefined') {
     el.id = `datepicker-${hash({wrapperStyle, holdingPen, label, placeholder, property, required, pattern, autofocus, permanentTopPlaceholder, onchange, oninput})}`
     el.isSameNode = target => el.id === target.id
-    flatpickr(el, Object.assign(flatpickrConfig, {wrap: true, disableMobile: true}))
+    flatpickr(el, Object.assign(flatpickrConfig, {wrap: true, disableMobile: true}, timeOnly ? {noCalendar: true, enableTime: true} : null))
   }
 
   return el
 }
 
-export default args => cache(datePicker, args)
+export default args => cache(dateTimePicker, args)
