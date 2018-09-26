7d789478773d228078baa299a8b2d08ca35ddc6f
lorengreenfield <loren@intereact.co.nz>
2018-09-26 13:22:10 +1200

    Since browsers can sort of bump scroll past the bottom of the page, you see the toaster, so the opacity is now also set to 0 when it's hidden below the screen.

--- components/toast.mjs
+++ components/toast.mjs
@@ -16,7 +16,7 @@ let styles = css`
   }
     
   .toastContainer{
-    transition: bottom 0.5s ease;
+    transition: bottom 0.5s ease, opacity 1s ease;
     width: 100%;
     height: 50px;
     position: fixed;
@@ -29,7 +29,7 @@ let styles = css`
   }
 `
 export default ({on, message, colour, multiline, closeAction}) => html`
-  <div class="${styles.toastContainer}" onclick=${() => geb.broadcast('errorDismissed')} style="${on ? 'bottom: 0px;' : `bottom: -${multiline ? '100' : '50'}px;`}${multiline ? 'height: 100px;' : ''}">
+  <div class="${styles.toastContainer}" onclick=${() => geb.broadcast('errorDismissed')} style="${on ? 'bottom: 0px;' : `bottom: -${multiline ? '100' : '50'}px; opacity: 0; `}${multiline ? 'height: 100px;' : ''}">
       <div class="${styles.toast}" style="background-color: ${colour || 'red'};">
           <div style="width: 100%; margin-left: 20px">${message}</div>
           <div style="margin-right: 10px; cursor: pointer;" onclick=${e => closeAction && closeAction(e)}>${close({colour: 'white'})}</div>
