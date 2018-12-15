93c84dc9d9eb5194deb195cda387741f8086b881
lorengreenfield <loren@intereact.co.nz>
2018-12-15 13:52:34 +1300

    Fixing button component to be inline we recent updates of other components

--- components/button.mjs
+++ components/button.mjs
@@ -7,15 +7,21 @@ let cache = new LRU(300)
 class Button extends Component {
   createElement (args) {
     this.args = clone(args)
-    let {width, minWidth, height, text, action, colour, disabled} = args
+    this.action = args.action
+    let {width, minWidth, height, text, colour, disabled} = args
 
     return html`
-        <button style="${disabled ? 'opacity: 0.3;' : ''}font-family: inherit; line-height: inherit; cursor: ${disabled ? 'not-allowed' : 'pointer'}; background-color: ${colour}; ${minWidth ? `min-width: ${minWidth};` : ''} border-radius: 3px; margin: 30px 10px; width: ${width}; height: ${height}; border: none; box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12); font-size: 17px; color: white;" ${disabled ? 'disabled' : ''} onclick=${e => action(e)}>${text}</button> 
+        <button style="${disabled ? 'opacity: 0.3;' : ''}font-family: inherit; line-height: inherit; cursor: ${disabled ? 'not-allowed' : 'pointer'}; background-color: ${colour}; ${minWidth ? `min-width: ${minWidth};` : ''} border-radius: 3px; margin: 30px 10px; width: ${width}; height: ${height}; border: none; box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12); font-size: 17px; color: white;" ${disabled ? 'disabled' : ''} onclick=${e => this.action(e)}>${text}</button> 
     `
   }
 
   update (args) {
     let diff = deepDiff.diff(this.args, args)
+    Object.keys(diff).forEach(key => {
+      if (typeof diff[key] === 'function') {
+        this[key] = args[key]
+      }
+    })
     return !!Object.keys(diff).find(key => typeof diff[key] !== 'function')
   }
 }
@@ -30,10 +36,11 @@ function button (args) {
       instance = new Button()
       cache.set(args.uniqueKey, instance)
     }
+    return instance.render(args)
   } else {
     instance = new Button()
+    return instance.createElement(args)
   }
-  return instance.render(args)
 }
 
 export default args => button(args)
