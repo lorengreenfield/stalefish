598ca6ca7800828d4fadad473be197cf90a53059
lorengreenfield <loren@intereact.co.nz>
2024-03-30 10:25:42 +1300

    Checkbox dark background option

--- components/checkbox.mjs
+++ components/checkbox.mjs
@@ -5,53 +5,51 @@ import clone from 'fast-clone'
 let cache = new LRU(300)
 
 let styles = css`
-  .checkbox {
-    margin: 0 10px 0 20px !important;
-    position: absolute;
-    right: 4px;
-    top: 16.5px;
-  }
-  
-  .label {
-    position: relative;
-    display: inline-flex; 
-    align-items: center;
-    border-radius: 0;
-    font-size: 18px; 
-    font-weight: normal; 
-    color: #999; 
-    margin: 5px 5px 5px 5px;
-    padding: 10px;
-    background-color: rgba(255,255,255,0.8);
-    border: solid 5px #c9c9c9;
-    user-select: none;
-    padding: 12px; 
-    box-sizing: border-box;
-    width: calc(100% - 10px);
-  }
-  
-  .checkbox.touched:invalid:not(:focus) {
-    outline: red solid 2px;
-  }
+    .checkbox {
+        margin: 0 10px 0 20px !important;
+        position: absolute;
+        right: 4px;
+        top: 16.5px;
+    }
+
+    .label {
+        position: relative;
+        display: inline-flex;
+        align-items: center;
+        border-radius: 0;
+        font-size: 18px;
+        font-weight: normal;
+        color: #999;
+        margin: 5px 5px 5px 5px;
+        border: solid 5px #c9c9c9;
+        user-select: none;
+        padding: 12px;
+        box-sizing: border-box;
+        width: calc(100% - 10px);
+    }
+
+    .checkbox.touched:invalid:not(:focus) {
+        outline: red solid 2px;
+    }
 `
 
 class Checkbox extends Component {
   createElement (args) {
     this.args = clone(args)
     this.onchange = args.onchange
-    let { wrapperStyle, holdingPen, label, property, required, indeterminate, disabled } = args
+    let { wrapperStyle, holdingPen, label, property, required, indeterminate, disabled, darkBackground } = args
 
     let checkboxEl = html`<input data-gramm="false" ${disabled ? { disabled } : ''} style="${disabled ? 'cursor: not-allowed;' : ''}" class="${styles.checkbox} ${fieldIsTouched(holdingPen, property) === true ? styles.touched : ''}" value="${holdingPen[property] === true ? 'true' : null}" ${holdingPen[property] === true ? { checked: 'checked' } : ''} onchange=${e => {
-      formField(holdingPen, property)(e)
-      this.onchange && this.onchange(e)
+        formField(holdingPen, property)(e)
+        this.onchange && this.onchange(e)
     }} type="checkbox" ${required ? { required: 'required' } : ''} />`
 
     checkboxEl.indeterminate = indeterminate || false
 
     return html`
-      <label style="${disabled ? 'cursor: not-allowed; opacity: 0.3;' : ''}text-align: left; width: 100%; display: inline-block; vertical-align: bottom;" ${wrapperStyle ? { 'class': wrapperStyle } : ''}>
-        <span class="${styles.label}">${label}${required ? ' *' : ''}${checkboxEl}</span>
-      </label>
+        <label style="${disabled ? 'cursor: not-allowed; opacity: 0.3;' : ''}text-align: left; width: 100%; display: inline-block; vertical-align: bottom;" ${wrapperStyle ? { 'class': wrapperStyle } : ''}>
+            <span class="${styles.label}" style="background-color: rgba(255,255,255,${darkBackground ? 1 : 0.8});">${label}${required ? ' *' : ''}${checkboxEl}</span>
+        </label>
     `
   }
 
