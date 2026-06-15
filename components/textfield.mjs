8b002defc09ff428d616ebb4dcbb6e7947e69e2b
lorengreenfield <loren@intereact.co.nz>
2026-06-15 17:41:43 +1200

    Fixing textfield bug on updates

--- components/textfield.mjs
+++ components/textfield.mjs
@@ -273,7 +273,7 @@ export default ({ highlightBorder = false, wrapperStyle = null, holdingPen, labe
                             style="${disabled ? 'cursor: not-allowed; opacity: 0.3;' : ''}"
                             id="${wrapperId}-input"
                             class="${styles.textfield} ${isNumber ? styles.withRightControls : ''} ${fieldIsTouched(holdingPen, property) === true ? styles.touched : ''} ${highlightBorder ? styles.highlight : ''}"
-                            value="${holdingPen[property] !== undefined && holdingPen[property] !== null ? holdingPen[property] : ''}"
+                            .value=${holdingPen[property] !== undefined && holdingPen[property] !== null ? holdingPen[property] : ''}
                             onkeyup=${e => { onkeyup && onkeyup(e); if (isNumber && e && e.key === 'ArrowUp') { e.preventDefault && e.preventDefault(); inc() } else if (isNumber && e && e.key === 'ArrowDown') { e.preventDefault && e.preventDefault(); dec() } }}
                             ?required=${required}
                             onchange=${e => { change({ e, holdingPen, property, label: styles.label }); onchange && onchange(e) }}
@@ -331,15 +331,15 @@ export default ({ highlightBorder = false, wrapperStyle = null, holdingPen, labe
               ${label ? html`<span class="${styles.label} ${normalizedType === 'color' ? styles.labelColorAdjustForColorType : ''}" style="opacity: ${holdingPen[property] === 0 || holdingPen[property] || (permanentTopPlaceholder || permanentTopLabel) ? 1 : 0}; font-size: 16px; font-weight: normal; color: #999; margin-left: 5px; padding: 9px; background-color: rgba(255,255,255,${darkBackground ? 1 : 0.8}); ">${label}${required ? ' *' : ''}</span>` : ''}
               ${input}
               ${
-                      (isNumber && !disabled)
-                              ? html`
+  (isNumber && !disabled)
+    ? html`
                                   <span id="${wrapperId}-ctrls" class="${styles.controls} ${hasValueContext ? styles.controlsWithValueContext : ''}" aria-hidden="false">
                         <button type="button" tabindex="-1" aria-label="Increase value" class="${styles.btn}" style="transform: rotate(180deg);" onmousedown=${(e) => e.preventDefault()} onclick=${inc}>${solidDown({ colour: '#CCC', width: '14px', height: '14px' })}</button>
                         <button type="button" tabindex="-1" aria-label="Decrease value" class="${styles.btn}" onmousedown=${(e) => e.preventDefault()} onclick=${dec}>${solidDown({ colour: '#CCC', width: '14px', height: '14px' })}</button>
                       </span>
                               `
-                              : ''
-              }
+    : ''
+}
           </label>
       </div>
   `
